c1be465e4799d16a8f272c73d8c946d359b4c79d
JITHIN K C <jithinkc22@gmail.com>
2020-05-20 18:31:22 +0530

    Add files via upload

---
+++ bakersy.m
@@ -0,0 +1,21 @@
+function [out]=bakersy(x,y,p,siz)
+
+n=siz*siz;
+
+
+for i=1:n
+
+    % If you need complete code contact me
+    
+   end
+
+end
+
+x1= reshape(x1,[siz,siz]);
+y1= reshape(y1,[siz,siz]);
+plot(y1,x1,'DisplayName','x1')
+out=y;
+
+
+end
+
---
+++ main.m
@@ -0,0 +1,19 @@
+% Main fuction -bakers
+%-------------------------------------------------------------------------
+% Note: If you are using my code for your system or project, you should always cite my paper as a reference.
+% K C.Jithin, SyamSankar, "Colour image encryption algorithm combining Arnold map, DNA sequence operation, and a Mandelbrot set",
+% Journal of Information Security and Applications, Elsevier, Volume 50, February 2020, DOI: https://doi.org/10.1016/j.jisa.2019.102428
+%--------------------------------------------------------------------------
+ % Calling chaotic map
+  x=  ; % Initial values
+    y=  ; % Initial values
+    p=;%parameter value
+   prompt=' Enter the one dimension of the chaotic map';
+       siz=input(prompt);
+%-------------------------------------------------------------------------- 
+  [out,o]=bakers(x,y,p,siz);
+   disp('Output :');
+   pause(1);
+   disp(out);
+   disp(o);
+%-------------------------------------------------------------------------
